455af4b246f5bec58e969b5c8d50cfe71e8ad1a1
pranav <pranavpppp1@gmail.com>
2022-12-22 00:37:20 -0800

    session completed

--- shopforpets/home/admin.py
+++ shopforpets/home/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import PetProduct
 
+class PetProductAdmin(admin.ModelAdmin):
+    list_display=('name','price','qty','img','desc','discount','date')
+
 # Register your models here.
 admin.site.register(PetProduct)
--- shopforpets/product/views.py
+++ shopforpets/product/views.py
@@ -12,9 +12,35 @@ def detail(request):
     data=PetProduct.objects.get(id=id)
     
     total=int(data.price)-(int(data.price)*int(data.discount)/100)
-    response=render(request,"detail.html",{"pro":data,"total":total})
-    response.set_cookie("price",data.price)
-    return response
+    if "his" in request.session:
+        if id in request.session["his"]:
+            request.session["his"].remove(id)    
+            request.session["his"].insert(0,id)  
+        else:
+            request.session["his"].insert(0,id)
+        if len(request.session["his"])>4:
+                request.session["his"].pop()
+        print(request.session["his"])
+        recent=PetProduct.objects.filter(id__in=request.session["his"])
+        print(recent)
+        request.session.modified=True
+        return render(request,"detail.html",{"pro":data,"total":total,"recent":recent})
+                
+
+        
+    else:
+        print("hello")
+        request.session["his"]=[id]
+        print(request.session["his"])
+        return render(request,"detail.html",{"pro":data,"total":total})
+        
+  
+    
+
+
+
+
+
 def cmt(request):
     comments=request.POST["comment"]
     name=request.POST["user"]
